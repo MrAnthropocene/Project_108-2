24bb0b8adc9d981ce31103833b6da9098e1808d5
Jesus Montes <montesj4@uabc.edu.mx>
2026-02-16 14:20:04 -0800

    Initial Commit

---
+++ Project_108-2/Project_108_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_108_2App.swift
+//  Project_108-2
+//
+//  Created by Saul Herrera on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct Project_108_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project_108-2Tests/Project_108_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Project_108_2Tests.swift
+//  Project_108-2Tests
+//
+//  Created by Saul Herrera on 2/16/26.
+//
+
+import Testing
+@testable import Project_108_2
+
+struct Project_108_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
